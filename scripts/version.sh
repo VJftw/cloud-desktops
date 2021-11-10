4411949d0d75abe8c453e1fada068765641eeabf
VJ Patel <VJftw@users.noreply.github.com>
2021-11-10 22:49:56 +0000

    Update version.sh

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 if [ "${CI:-false}" == "true" ]; then
     # CI, therefore real
     echo "$(date --universal '+%Y-%m-%d')-$(git rev-parse --short HEAD)"
+else
+    # testing
+    echo "$(date --universal '+%s')-$(git rev-parse --short HEAD)-test"
 fi
-
-# testing
-echo "$(date --universal '+%s')-$(git rev-parse --short HEAD)-test"
