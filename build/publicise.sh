aa61bb5101bad782f17d9f578064bfd36c14a298
VJ Patel <vj@vjpatel.me>
2022-09-22 11:57:04 -0400

    use the method for artifact_ids to publicise.sh from release_body.sh

--- build/publicise.sh
+++ build/publicise.sh
@@ -6,10 +6,11 @@ JQ="$(dirname $0)/third_party/binary/jq"
 gcp_project="${GCP_PROJECT_ID:=}"
 
 packer_manifests=($(find ./plz-out -name 'packer-manifest.json'))
+
 artifact_ids=()
 for packer_manifest in "${packer_manifests[@]}"; do
     printf "inspecting packer manifest '%s'\n" "${packer_manifest}"
-    artifact_ids+=($($JQ -r '.builds[] | select(.name == "googlecompute") | .artifact_id' "${packer_manifest}"))
+    artifact_ids+=($($JQ -r '.builds[].artifact_id' "${packer_manifest}"))
 done
 
 for artifact_id in "${artifact_ids[@]}"; do
